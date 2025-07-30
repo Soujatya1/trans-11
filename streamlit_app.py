4e11314bb5ff2fb3e91b385f7106c09fb4340588
Soujatya Das <soujatya548@gmail.com>
2025-07-30 19:15:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from docx.enum.text import WD_COLOR_INDEX
 import re
 
 def add_context(text, source_language, target_language):
-    if source_language == source_language and target_language == target_language:
+    if source_language == source_lang and target_language == target_language:
         patterns = {
             r"Reply to your notice": "Response to your notice",
             r"Reply to the notice": "Response to the notice", 
